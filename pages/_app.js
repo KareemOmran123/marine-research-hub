132c6ca75fb666cb01af30a7c99dba2ca7dcf028
KareemOmran123 <koko7sky@gmail.com>
2025-06-06 22:28:17 -0500

    theme button
    new backgrounds. you can now have a floating aquarium background!

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
 import Head from 'next/head';
 import "@/styles/globals.css";
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }) {
+  const [theme, setTheme] = useState('default'); // 'default' or 'aquarium'
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'default' ? 'aquarium' : 'default'));
+  };
+
   return (
     <>
       <Head>
-        <title>Marine Research Hub</title> {/* Change this to your desired title */}
+        <title>Marine Research Hub</title>
       </Head>
-      <Component {...pageProps} />
+      <div className={theme === 'aquarium' ? 'aquarium-theme' : ''}>
+        <button
+          onClick={toggleTheme}
+          className="absolute top-6 left-6 bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+        >
+          {theme === 'default' ? 'Switch to Aquarium Theme' : 'Switch to Default Theme'}
+        </button>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
